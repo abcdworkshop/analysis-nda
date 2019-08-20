6dd2384e468d31b0e34aaa0698f28395ca6368c9
Dani Cosme <dani.cosme@gmail.com>
2019-08-20 10:14:15 -0700

    added more info re:regular expressions

--- notebooks/general/config.R
+++ notebooks/general/config.R
@@ -4,11 +4,13 @@
 # data_dir = path to directory containing the curated data
 # script_dir = path to scripts directory
 # output_dir = path to directory to write output data files
-# filter_list = regular expression to filter text file list with
+# filter_list = regular expression to filter text file list with. 
+# 	This statement will be used as an input to grepl(); to match more than one 
+#	term, use | between terms (see example below)
 
 # OBS! Make sure not to place/write the data to your repository
 
 data_dir = "~/Desktop/data"
 script_dir = "~/Documents/code/analysis-nda/notebooks/general"
 output_dir = "~/Desktop/data_out"
-filter_list = "nback"
+filter_list = "nback|mid"
